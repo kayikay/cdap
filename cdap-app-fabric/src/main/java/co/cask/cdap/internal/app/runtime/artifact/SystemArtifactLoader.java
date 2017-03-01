06196b9b31975832fbc7d9c19a1b5b732a5467c9
shankar <shankar@cask.co>
2017-03-01 15:03:28 -0800

    CDAP-7628 system artifact loader thread logging

--- cdap-app-fabric/src/main/java/co/cask/cdap/internal/app/runtime/artifact/SystemArtifactLoader.java
+++ cdap-app-fabric/src/main/java/co/cask/cdap/internal/app/runtime/artifact/SystemArtifactLoader.java
@@ -16,12 +16,16 @@
 
 package co.cask.cdap.internal.app.runtime.artifact;
 
+import co.cask.cdap.common.logging.LogSamplers;
+import co.cask.cdap.common.logging.Loggers;
 import co.cask.cdap.common.service.RetryOnStartFailureService;
 import co.cask.cdap.common.service.RetryStrategies;
 import com.google.common.base.Supplier;
 import com.google.common.util.concurrent.AbstractService;
 import com.google.common.util.concurrent.Service;
 import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
@@ -31,6 +35,8 @@ import java.util.concurrent.TimeUnit;
  */
 public final class SystemArtifactLoader extends AbstractService {
   private final Service serviceDelegate;
+  private static final Logger LOG = LoggerFactory.getLogger(SystemArtifactLoader.class);
+  private static final Logger ERROR_LOG = Loggers.sampling(LOG, LogSamplers.onceEvery(10));
 
   @Inject
   SystemArtifactLoader(final ArtifactRepository artifactRepository) {
@@ -42,10 +48,12 @@ public final class SystemArtifactLoader extends AbstractService {
           protected void doStart() {
             try {
               artifactRepository.addSystemArtifacts();
+              LOG.info("Successfully added system artifacts");
               // if there is no exception, all good, continue on
               notifyStarted();
             } catch (Exception e) {
               // transient error, fail it and retry
+              ERROR_LOG.warn("Exception while adding system artifacts", e);
               notifyFailed(e);
             }
           }
